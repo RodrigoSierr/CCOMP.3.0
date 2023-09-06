abb1ecb673828a21e2e1bf16fc3e59e0c6334c49
RodrigoSierr <101744217+RodrigoSierr@users.noreply.github.com>
2023-09-05 21:10:39 -0500

    Add files via upload

---
+++ Tarea_1/20/24.cpp
@@ -0,0 +1,26 @@
+/*#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x{ 0 };
+	int y{ 0 };
+	int suma;
+	cout << "Defina un valor para x: ";
+	cin >> x;
+
+	cout << "Defina un valor para y: ";
+	cin >> y;
+
+	suma = x + y;
+
+	cout << "La suma es: " << suma << "\n";
+
+	if (suma % 2 == 0) {
+		cout << "la suma es par" << "\n";
+	}
+	else {
+		cout << "la suma se impar" << "\n";
+	}
+	return 0;
+}*/
---
+++ Tarea_1/20/25.cpp
@@ -0,0 +1,25 @@
+/*#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	int fa;
+
+	cout << "Dar un valor para a: ";
+	cin >> a;
+	cout << "Dar un valor para b: ";
+	cin >> b;
+	cout << "Dar un valor para c: ";
+	cin >> c;
+
+	fa = a * b;
+	if ((fa % c) == 0) {
+		cout << "a y b son factores de c" << "\n";
+	}
+	else {
+		cout << "a y b no son factores de c" << "\n";
+	}
+
+	return 0;
+}*/
