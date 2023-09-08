cdb9a195b252b2ee4d5bc5395713935a251512c7
RodrigoSierr <101744217+RodrigoSierr@users.noreply.github.com>
2023-09-07 19:17:46 -0500

    Update Ferreteria.h

--- Tarea_2/Ferreteria.h
+++ Tarea_2/Ferreteria.h
@@ -1,18 +1,19 @@
+#pragma once
 #include <string>
 #include <iostream>
 
 using namespace std;
 
-class Ferreteria{
-    private:
+class Ferreteria {
+private:
     string num_pieza;
     string des_pieza;
     int cant_pieza;
     int prec_art;
-    double vat {0.2};
-    double desc {0};
-    
-    public:
+    double vat{ 0.2 };
+    double desc{ 0 };
+
+public:
     Ferreteria()
     {
         num_pieza = "";
@@ -22,52 +23,64 @@ class Ferreteria{
         vat = 0;
         desc = 0;
     }
-     Ferreteria(std::string numpieza, std::string despieza, int cantpieza, int precart, double vat_, double desc_)
+    Ferreteria(std::string numpieza, std::string despieza, int cantpieza, int precart, double vat_, double desc_)
         : num_pieza(numpieza), des_pieza(despieza), cant_pieza(cantpieza), prec_art(precart), vat(vat_), desc(desc_) {
     }
-
-    void vender(double monto){
-        
+    void setnum_pieza(string numpieza) {
+         num_pieza = numpieza;
     }
-
-    void setnum_pieza (string numpieza){
-        num_pieza = numpieza;
-    }
-    string getnum_pieza(){
-        return num_pieza;
-    }
-    void setdes_pieza(string despieza){
+    void setdes_pieza(string despieza) {
         des_pieza = despieza;
     }
-    string getdes_pieza(){
-        return des_pieza;
+    void setcant_pieza(int cantpieza) {
+        if (cantpieza > 0) {
+            cant_pieza = cantpieza;
+        }
     }
-    void setcant_pieza(int cantpieza){
-        cant_pieza = cantpieza;
-    }
-    int getcant_pieza(){
-        return cant_pieza;
-    }
-    void setprec_art(int precart){
-        prec_art = precart;
-    }
-    int getprec_art(){
-        return prec_art;
-    }
-    void setvat(double vat_){
-        vat = vat_;
-    }
-    double getvat(){
-        return vat;
-    }
-    void setdesc(double desc_){
-        desc = desc_;
-    }
-    double getdesc(){
-        return desc;
-    }
-    double monto(int total){
-        total = (cant_pieza * prec_art) / vat; 
+    void setprec_art(int precart) {
+        if (precart > 0) {
+            prec_art = precart;
+        }
     }
 
+    void setdesc(double desc_) {
+        if (desc_ > 0) {
+            desc = desc_;
+        }
+    }
+    void setvat(double vat_) {
+        if (vat_ > 0) {
+            vat = vat_;
+        }
+    }
+    string getnum_pieza() {
+        return num_pieza;
+    }
+    string getdes_pieza() {
+        return des_pieza;
+    }
+    
+    int getcant_pieza() {
+        return cant_pieza;
+    }
+    
+    int getprec_art() {
+        return prec_art;
+    }
+    
+    double getvat() {
+        return vat;
+    }
+    
+    double getdesc() {
+        return desc;
+    }
+    
+    double getnvoiceAmount() {
+        double recibo = (cant_pieza * prec_art) + vat;
+        return recibo;
+    }
+
+    
+    
 };
