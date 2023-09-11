962ce7a4f5c9fd1cb4d060c087679253ba4c6cb1
RodrigoSierr <101744217+RodrigoSierr@users.noreply.github.com>
2023-09-10 20:57:07 -0500

    Update Ferreteria.cpp

--- Tarea_2/Ferreteria.cpp
+++ Tarea_2/Ferreteria.cpp
@@ -5,20 +5,20 @@
 using namespace std;
 
 int main() {
-    Ferreteria factura{ "123", "Pieza A", 10, 25, 0.2, 5.0 };
+    Ferreteria factura{ "2", "Martillo", 2, 25, 0.2, 5.0 };
     cout << "El numero de piezas es: " << factura.getnum_pieza() << endl;
     cout << "La Descripcion de la pieza es: " << factura.getdes_pieza() << endl;
     cout << "La cantidad de articulos es: " << factura.getcant_pieza() << endl;
     cout << "El precio del articulo seria: " << factura.getprec_art() << endl;
     cout << "El impuesto es: " << factura.getvat() << endl;
     cout << "El descuento es: " << factura.getdesc() << endl;
-    
-    
-        double importeRecibo = factura.getnvoiceAmount(); 
 
-        cout << "Importe del recibo: " << importeRecibo << endl;
 
-        return 0;
+    double importeRecibo = factura.getnvoiceAmount();
+
+    cout << "Importe del recibo: " << importeRecibo << endl;
+
+    return 0;
 
 
 }
