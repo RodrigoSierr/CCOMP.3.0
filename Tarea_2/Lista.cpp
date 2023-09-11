a00061d60301c70e85b4e761499e87a9c965ad79
RodrigoSierr <101744217+RodrigoSierr@users.noreply.github.com>
2023-09-10 20:45:34 -0500

    Create Lista.cpp

---
+++ Tarea_2/Lista.cpp
@@ -0,0 +1,11 @@
+#include <iostream>	
+#include "Lista.h"
+#include <string>
+
+using namespace std;
+
+int main() {
+	lista MiLista(75176262, "Rodrigo", "Sierra", 1.1);
+	MiLista.display();
+	return 0;
+}
