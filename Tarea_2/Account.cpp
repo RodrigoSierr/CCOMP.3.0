15f88091cd438a771f4b896f30f5a6a45118e42e
RodrigoSierr <101744217+RodrigoSierr@users.noreply.github.com>
2023-09-10 20:03:34 -0500

    Create Account.cpp

---
+++ Tarea_2/Account.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include "Account.h"
+using namespace std;
+
+int main() {
+    Account user1{ "Rodrigo", -7 , 0 };
+    user1.Display();
+
+    return 0;
+}
