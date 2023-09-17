6421e4d8dab41e0b32f34d177328ec76f0f97516
RodrigoSierr <101744217+RodrigoSierr@users.noreply.github.com>
2023-09-17 13:32:05 -0500

    Update 1.cpp

--- Proyecto_Euler/1.cpp
+++ Proyecto_Euler/1.cpp
@@ -1,17 +1,17 @@
+/*Si enumeramos todos los números naturales por debajo de 10 que son múltiplos de 3 o 5, obtenemos 3, 5, 6 y 9. La suma de estos múltiplos es 23.
+Encuentra la suma de todos los múltiplos de 3 o 5 por debajo de 1000.*/
+
 #include <iostream>
+
 using namespace std;
 
 int main(){
-int x, suma;
-cout << "Agregue un valor para x: ";
-cin >> x;
+    int suma = 0;
+        for (int i = 0; i < 1000; i++)
+            if(i % 3 == 0 || i % 5 == 0){
+            suma += i;
+                }
+    cout << suma << endl;
 
-suma = suma + x;
-while(1000 < 0){
-    if ((x % 3 == 0)||(x % 5 == 0)){
-        cout << suma;
-    } suma++;
-    
-}
-return 0;
+    return 0;
 }
