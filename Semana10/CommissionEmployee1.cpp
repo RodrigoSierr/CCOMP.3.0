8a2f90ea03350450b8b8f84e94811c33df371050
ALUMNO-N412 <Alumno-N412@UCSP.AULAB>
2023-11-02 08:11:31 -0500

    ga

---
+++ Semana10/CommissionEmployee1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+#include "CommissionEmployee.h"
+using namespace std;
+
+int main(){
+    CommissionEmployee{"Sue", "Jones", "222-22-2222", 10000, .06};
+
+    cout <<
+}
